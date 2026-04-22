16221df554554c04d13c33e6811f424a12611636
Patricia Herman Cabantog <phcabantog@paterostechnologicalcollege.edu.ph>
2026-04-23 01:40:51 +0800

    update corssss

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,6 +17,7 @@ const { makeErrorId } = require("./utils/response");
 const app = express();
 
 const defaultAllowedOrigins = [
+  "https://ducksitet.vercel.app/",
   "https://ducksite-ne14006pn-patricias-projects-ee9e3b1c.vercel.app",
   "http://localhost:3000",
 ];
